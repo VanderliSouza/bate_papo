9cf42e0e38cf5053f31ef4c24d46244c9b2da602
VanderliSouza <vandsouz@gmail.com>
2024-05-25 19:45:35 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# bate_papo
-Bate-papo casual do assunto de seu interesse
+# AskME - GenAI chatbot
+
+O Bate-papo é um chatbot que utiliza um modelo generativo para gerar respostas aos dados introduzidos pelo utilizador. O modelo é treinado num grande conjunto de dados de conversas e é capaz de gerar respostas coerentes e contextualmente relevantes. O chatbot é capaz de compreender e responder a uma vasta gama de tópicos e pode manter uma conversa com o utilizador durante um longo período de tempo. O chatbot também é capaz de gerar respostas que são adaptadas à entrada do utilizador, tornando-o uma ferramenta altamente eficaz para interagir com os utilizadores de uma forma natural e conversacional.
+
---
+++ main.py
@@ -0,0 +1,40 @@
+import streamlit as st
+import os
+import google.generativeai as genai
+
+# Initialize Gemini-Pro
+genai.configure(api_key=st.secrets["GOOGLE_GEMINI_KEY"])
+model = genai.GenerativeModel('gemini-pro')
+
+# Gemini uses 'model' for assistant; Streamlit uses 'assistant'
+def role_to_streamlit(role):
+  if role == "model":
+    return "assistant"
+  else:
+    return role
+
+# Add a Gemini Chat history object to Streamlit session state
+if "chat" not in st.session_state:
+    st.session_state.chat = model.start_chat(history = [])
+
+# Display Form Title
+st.set_page_config(page_title="Bate-papo")
+st.title("Bate-papo: Em que posso ajudar?")
+
+# Display chat messages from history above current input box
+for message in st.session_state.chat.history:
+    with st.chat_message(role_to_streamlit(message.role)):
+        st.markdown(message.parts[0].text)
+
+# Accept user's next message, add to context, resubmit context to Gemini
+if prompt := st.chat_input("Em que posso ajudar? Sobre o que vamos conversar hoje?"):
+    # Display user's last message
+    st.chat_message("user").markdown(prompt)
+    
+    # Send user entry to Gemini and read the response
+    response = st.session_state.chat.send_message(prompt) 
+    
+    # Display last 
+    with st.chat_message("assistant"):
+        st.markdown(response.text)
+
